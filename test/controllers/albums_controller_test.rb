2cb92d4ceab855f4bb80724f391d74b98cedca62
Lucas Sousa <ls442336@gmail.com>
2025-03-17 18:13:43 -0300

    Remove unused tests

--- test/controllers/albums_controller_test.rb
+++ test/controllers/albums_controller_test.rb
@@ -1,8 +1,4 @@
 require "test_helper"
 
 class AlbumsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get albums_index_url
-    assert_response :success
-  end
 end
--- test/controllers/games/quiz_items/guess_controller_test.rb
+++ test/controllers/games/quiz_items/guess_controller_test.rb
@@ -1,8 +1,4 @@
 require "test_helper"
 
 class Games::QuizItems::GuessControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get games_quiz_items_guess_create_url
-    assert_response :success
-  end
 end
